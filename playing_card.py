ab63b9a3257268909b32b1f52cc8cd64984ec9d1
RobbieAuld <40382956+RobbieAuld@users.noreply.github.com>
2020-07-05 08:26:43 +0100

    completed playing card
    example of _init_ _len_ and _get_item_
    also examples of sorted on key function,  reversed. Implements collections.namedtuple

--- playing_card.py
+++ playing_card.py
@@ -6,10 +6,14 @@ Card = collections.namedtuple("Card", ["rank", "suit"])
 
 class FrenchDeck:
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = "spades hearts diamonds clubs".split()
+    suits = {"spades": 3, "hearts": 2, "diamonds": 1, "clubs": 0}
+
+    def spades_high(self, card):
+        rank_value = self.ranks.index(card.rank)
+        return rank_value * len(self.suits.values()) + self.suits[card.suit]
 
     def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
+        self._cards = [Card(rank, suit) for suit in self.suits.keys() for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
@@ -21,5 +25,5 @@ class FrenchDeck:
 if __name__ == "__main__":
     deck = FrenchDeck()
 
-    [print(card) for card in reversed(deck)]
-    
+    [print(card) for card in sorted(deck, key=deck.spades_high)]
+
